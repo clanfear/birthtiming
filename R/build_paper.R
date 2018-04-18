9c1790ffe6b1480154fda42a3d60016bb5471e2e
Charles Lanfear <cclanfear@gmail.com>
2018-04-17 17:24:29 -0700

    Update build_paper.R

--- R/build_paper.R
+++ R/build_paper.R
@@ -1,3 +1,25 @@
 build_birthtiming <- function(){
   knitr::knit(paste0(.libPaths(), "/birthtiming/paper/paper.Rmd"))
 }
+
+open_paper_dir <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  if(tolower(os)=="windows"){
+    shell(paste0("explorer ",
+                 stringr::str_replace_all(.libPaths(), "/", "\\\\\\" ),
+                 "\\birthtiming\\paper"), intern=TRUE)
+  } else if (tolower(os)=="osx"){
+    system2("open", paste0(.libPaths(), "/birthtiming/paper"))
+  }
+}
