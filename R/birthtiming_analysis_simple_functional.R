4865816a721c7d7bf47cae63c3ac0ab0be2093b4
Charles Lanfear <cclanfear@gmail.com>
2019-02-21 13:09:18 -0800

    Removing under development syntax
    Migrating things under development into inst so paper compiles correctly.

---
+++ R/birthtiming_analysis_simple_functional.R
@@ -0,0 +1,57 @@
+####
+# Things to do"
+## Set cov=0 for instrument, check changes in params
+## Drop cov(age, shared) and look at changes (this is RE)
+## Consider disabled children
+
+run_naive_sem <- function(){
+  diff_model_naive <-
+    '
+    f1 =~ 1*difficulty_1 + 1*difficulty_2
+
+    difficulty_1 ~ bage*m_age_at_birth_1
+    difficulty_2 ~ bage*m_age_at_birth_2
+
+    m_age_at_birth_1~~f1
+    m_age_at_birth_2~~f1
+    m_age_at_birth_1~~m_age_at_birth_2
+
+    difficulty_1~~m_age_at_birth_2
+    '
+
+  diff_model_naive_out <- lavaan::sem(model=diff_model_naive,
+                                      data=gen_birthtiming_data(min_children=2, max_children=10) %>%
+                                        filter(!is.na(difficulty_1) & !is.na(difficulty_2)))
+  naive_tidy <- lavaan::parameterEstimates(diff_model_naive_out, standardized = T)
+  return(naive_tidy)
+}
+#
+# run_full_sem <- function(){
+#   diff_model_full <-
+#     '
+#   f1 =~ 1*difficulty_1 + 1*difficulty_2
+#   f2 =~ 1*c_birth_weight_oz_1 + 1*c_birth_weight_oz_2
+#
+#   difficulty_1 ~ bage1*m_age_at_birth_1 +  sex_1 + Parity_1 + gestation_time_1
+#   difficulty_2 ~ bage1*m_age_at_birth_2 +  sex_2 + Parity_2 + gestation_time_2
+#   c_birth_weight_oz_1 ~ bage2*m_age_at_birth_1 +  sex_1 + Parity_1 + gestation_time_1
+#   c_birth_weight_oz_2 ~ bage2*m_age_at_birth_2 +  sex_2 + Parity_2 + gestation_time_2
+#
+#   m_age_at_birth_1~~f1 + f2 + sex_1 + gestation_time_1 + Parity_1
+#   m_age_at_birth_2~~f1 + f2 + sex_2 + gestation_time_2 + Parity_2
+#   m_age_at_birth_1~~m_age_at_birth_2
+#
+#
+#   difficulty_1~~m_age_at_birth_2
+#   c_birth_weight_oz_1~~m_age_at_birth_2
+#   '
+#
+#   diff_model_full_out <- lavaan::sem(model=diff_model_full,
+#                                      data=gen_birthtiming_data(min_children=2, max_children=10, standardize=TRUE) %>%
+#                                        filter(!is.na(difficulty_1) & !is.na(difficulty_2)), missing = "ML")
+#   full_tidy <- lavaan::parameterEstimates(diff_model_full_out, standardized = T)
+#   return(full_tidy)
+# }
+
+##
+# Hazard Model
